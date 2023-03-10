902f4837161c850e05e925b10f16bfd789271167
sivm99 <git.v7gr4@simplelogin.fr>
2023-03-10 10:45:32 +0530

    java practical 4 added

---
+++ Java Programming/Lab/patterns.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//practical 4
+//we want to display the following pattern
+/* 
+1
+01
+101
+0101
+10101
+*/
+
+public class patterns {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
